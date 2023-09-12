c6cefa81da1d3aa29c85eeb61610a0552cead1f4
zaeem <zaeembaloch1198@example.com>
2023-09-13 01:28:16 +0500

    git commit --amend

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use actix_web::{web::Data, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
+use serde::Deserialize;
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
-
+use regex::Regex;
 
 pub struct AppState {
     db: Pool<Postgres>,
 }
+
+#[derive(Deserialize)]
+
+pub struct UserDataStruct {
+  username:String,
+  email:String,
+  password:String
+}
+
+impl UserDataStruct {
+    // Custom validation function for email
+    fn is_valid_email(&self) -> bool {
+        // Regular expression pattern for a simple email validation
+        let re = Regex::new(r"^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$").unwrap();
+        re.is_match(&self.email)
+    }
+
+    // Custom validation function for password
+    fn is_valid_password(&self) -> bool {
+        // Password validation criteria (e.g., minimum length)
+        self.password.len() >= 8
+    }
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -21,6 +46,7 @@ async fn main() -> std::io::Result<()> {
         App::new()
             .app_data(Data::new(AppState { db: pool.clone() }))
             .route("/users", actix_web::web::get().to(fetch_users))
+            .route("/create-user", actix_web::web::post().to(create_user))
             .route("/", actix_web::web::get().to(hello))
     })
     .bind(("127.0.0.1", 8080))?
@@ -29,7 +55,7 @@ async fn main() -> std::io::Result<()> {
 }
 
 async fn fetch_users(data: Data<AppState>) -> HttpResponse {
-    let query_result = sqlx::query_as::<_, (i32, String)>("SELECT id, name FROM mytable")
+    let query_result = sqlx::query_as::<_, (i32, String, String, String)>("SELECT * FROM users")
         .fetch_all(&data.db)
         .await;
 
@@ -41,6 +67,38 @@ async fn fetch_users(data: Data<AppState>) -> HttpResponse {
         }
     }
 }
+
+async fn create_user(user_data: actix_web::web::Json<UserDataStruct>, data: Data<AppState>) -> HttpResponse {
+    let username = &user_data.username;
+    let email = &user_data.email;
+    let password = &user_data.password;
+
+    if !user_data.is_valid_email() {
+        return HttpResponse::BadRequest().json("invalid email")
+    }
+    if !user_data.is_valid_password() {
+        return HttpResponse::BadRequest().json("invalid password")
+    }
+    
+    // Execute an SQL insert query to add the new user to the "users" table
+    let query_result = sqlx::query(
+        "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)"
+    )
+    .bind(username)
+    .bind(email)
+    .bind(password)
+    .execute(&data.db)
+    .await;
+
+    match query_result {
+        Ok(_) => HttpResponse::Ok().body("User created successfully"),
+        Err(err) => {
+            eprintln!("Database error: {:?}", err);
+            HttpResponse::InternalServerError().json("Internal Server Error")
+        }
+    }
+}
+
 
 async fn hello() -> HttpResponse {
     HttpResponse::Ok().body("Hello, World!")
