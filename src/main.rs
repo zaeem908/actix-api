d25692507e97d25f91d8a54c1971dbc30637fabc
zaeem <zaeembaloch1198@example.com>
2023-09-14 20:48:24 +0500

    added login functionality and jwt

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use actix_web::{web::Data, App, HttpResponse, HttpServer};
 use dotenv::dotenv;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 use regex::Regex;
-use bcrypt::{hash, verify, DEFAULT_COST};
+use bcrypt::{hash, DEFAULT_COST};
+extern crate jsonwebtoken as jwt;
+use jwt::{Header,  Algorithm, Validation, encode, decode, EncodingKey};
+
 
 
 pub struct AppState {
@@ -19,10 +22,13 @@ pub struct UserDataStruct {
 }
 
 #[derive(Deserialize)]
-
 pub struct LoginStruct {
     email:String,
     password:String
+}
+#[derive(Debug, Serialize, Deserialize)]
+struct Claims {
+    sub : String
 }
 
 pub fn hash_password(pwd:&str) -> Result<String, bcrypt::BcryptError> {
@@ -77,32 +83,47 @@ async fn fetch_users(data: Data<AppState>) -> HttpResponse {
     }
 }
 
-
-async fn login(user_data: actix_web::web::Json<LoginStruct>, data:Data<AppState>) -> HttpResponse {
+async fn login(user_data: actix_web::web::Json<LoginStruct>, data: Data<AppState>) -> HttpResponse {
     let email = &user_data.email;
     let password = &user_data.password;
-    let query_result = sqlx::query_as::<_, (String, String)>("SELECT email,password from users WHERE email = $1")
+
+
+    let query_result = sqlx::query_as::<_, (String, String, String)>("SELECT username, email, password FROM users WHERE email = $1")
         .bind(email)
         .fetch_optional(&data.db)
         .await;
 
     match query_result {
-        Ok(Some((stored_email, stored_password))) => {
+        Ok(Some((stored_username,stored_email, stored_password))) => {
+            println!("Found user with email: {}, stored password: {}", stored_email, stored_password);
+
             if bcrypt::verify(password, &stored_password).unwrap_or(false) {
-                  HttpResponse::Ok().json("user logged in")
-            }else {
+                let secret_key = std::env::var("SECRET_KEY").expect("SECTET_KEY must be set");
+
+                let token = encode(
+                    &Header::default(),
+                    &Claims { sub: email.to_string() },
+                    &EncodingKey::from_secret(secret_key.as_ref()),
+                );
+                println!("Generated token: {:?}", token);
+                println!("user {} is logged in succesfully", stored_username);
+                HttpResponse::Ok().json("succesfully logged in!")
+            } else {
                 HttpResponse::Unauthorized().json("Invalid credentials")
             }
         }
-        Ok(none) => {
-            HttpResponse::Unauthorized().json("no user found with that email!")
+        Ok(None) => {
+            HttpResponse::Unauthorized().json("No user found with that email!")
         }
         Err(err) => {
-            eprintln!("error: {:?}", err);
+            eprintln!("Error: {:?}", err);
             HttpResponse::InternalServerError().json("Internal Server Error")
         }
     }
 }
+
+
+
 
 async fn create_user(user_data: actix_web::web::Json<UserDataStruct>, data: Data<AppState>) -> HttpResponse {
     let username = &user_data.username;
